76c1506b92c8c4423d3a4496fe6af5bea9556cbb
Umair Khan <umairkhan814@icloud.com>
2022-04-04 17:03:18 -0500

    Add files via upload

---
+++ src/components/SubHeading/SubHeading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { images } from "../../constants";
+
+const SubHeading = () => (
+  <div style={{ marginBottom: "1rem" }}>
+    <p className="p__comorant">TITLE</p>
+    <img src={images.spoon} alt="spoon" className="" />
+  </div>
+);
+
+export default SubHeading;
